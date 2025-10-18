ff60b4ba3b61f23cf082ece3a345dfe88c3a2355
TridibeshSam31 <tridisam651@gmail.com>
2025-10-18 15:02:42 +0530

    complete

--- ServerlessArchitecture/started/src/index.ts
+++ ServerlessArchitecture/started/src/index.ts
@@ -6,4 +6,19 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
+  return c.text('Hello Hono!')
+})
+
 export default app
+
+
+//c is for context 
+
+
+
